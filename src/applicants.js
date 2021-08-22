61b6b372c2a70f8eaac7e1de528fc34e363719fe
Dan Boudet <dboudet04@gmail.com>
2021-08-22 18:26:18 -0400

    updated add applicant query

--- src/applicants.js
+++ src/applicants.js
@@ -38,12 +38,20 @@ exports.addNewApplicant = (req, res) => {
   const formData = req.body
   console.log(formData)
   db.query(
-    `INSERT INTO applicants(first_name,last_name,email,position,photo_url) VALUES ("${formData.firstName}","${formData.lastName}","${formData.email}","${formData.position}","${formData.photoUrl}");`,
+    `INSERT INTO applicants(first_name,last_name,email,position,photo_url,resume_url,notes) VALUES(?,?,?,?,?,?,?);`,
+    [
+      formData.firstName,
+      formData.lastName,
+      formData.email,
+      formData.position,
+      formData.photoUrl,
+      formData.resumeUrl,
+      formData.notes,
+    ],
     (err, rows) => {
       if (err) {
         console.log(err)
         res.status(500).send(err)
-        return
       }
       res.send({
         status: 201,
@@ -63,25 +71,32 @@ exports.updateApplicant = (req, res) => {
   db.query(
     `UPDATE applicants 
     SET 
-      first_name = "${formData.firstName}", 
-      last_name = "${formData.lastName}", 
-      photo_url = "${formData.photoUrl}", 
-      email = "${formData.email}", 
-      position = "${formData.position}", 
-      notes = "${formData.notes}",
+      first_name = ?, 
+      last_name = ?, 
+      photo_url = ?, 
+      email = ?, 
+      position = ?, 
+      notes = ?,
       score = ${formData.score},
       application_stage = ${formData.applicationStage}
     WHERE id = ${formData.id};`,
+    [
+      formData.firstName,
+      formData.lastName,
+      formData.photoUrl,
+      formData.email,
+      formData.position,
+      formData.notes,
+    ],
     (err, rows) => {
       if (err) {
         console.log(err)
         res.status(500).send(err)
-        return
       }
       res.send({
         status: 201,
         success: true,
-        message: "Successfully updated applicant",
+        message: "Applicant updated",
       })
     }
   )
