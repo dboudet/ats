8499138a5b14830426f05a72cb3054a3d561b3b5
Dan Boudet <dboudet04@gmail.com>
2024-02-22 21:00:05 -0500

    deploy API via AWS lambda

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors")
 const app = express()
 app.use(cors())
 app.use(express.json())
+const serverless = require('serverless-http');
 
 const {
   getAllApplicants,
@@ -23,6 +24,8 @@ app.get("/ats/applicants", getAllApplicants)
 // app.get("/ats/login", userLogin)
 // app.post("/ats/add-user", addUser)
 // app.patch("/ats/update-profile", updateUser)
+
+module.exports.handler = serverless(app);
 
 app.listen(3306, () => {
   console.log("Listening on port 3306 ...")
